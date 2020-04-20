346ac69b910259f6bb4882021173f5c96f0ad23c
Glitch (twitterbot) <none>
2020-04-20 13:41:19 +0000

    Added license and a link to twitterbot-cron.

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
 /* Setting things up. */
-var express = require('express'),
-    app = express(),   
-    Twit = require('twit'),
-    config = {
-    /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */      
-      twitter: {
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-        access_token: process.env.TWITTER_ACCESS_TOKEN,
-        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-      }
-    },
-    T = new Twit(config.twitter);
+const express = require( 'express' ),
+      app = express(),   
+      Twit = require( 'twit' ),
+      config = {
+      /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */      
+        twitter: {
+          consumer_key: process.env.TWITTER_CONSUMER_KEY,
+          consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+          access_token: process.env.TWITTER_ACCESS_TOKEN,
+          access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+        }
+      },
+      T = new Twit( config.twitter );
 
-app.use(express.static('public'));
+app.use( express.static( 'public' ) );
 
-/* You can use cron-job.org, uptimerobot.com, or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
+/* You can use cron-job.org, uptimerobot.com, or a similar site to periodically visit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
 
-app.all(`/${process.env.BOT_ENDPOINT}`, function(req, res){
+app.all( `/${process.env.BOT_ENDPOINT}`, function( req, res ){
+
   /* The example below tweets out "Hello world!". */
-  T.post('statuses/update', { status: 'hello world 👋' }, function(err, data, response) {
-    if (err){
-      console.log('error!', err);
-      res.sendStatus(500);
+  
+  T.post( 'statuses/update', { status: 'hello world 👋' }, function( err, data, response ) {
+    if ( err ){
+      console.log( 'error!', err );
+      res.sendStatus( 500 );
     }
     else{
-      res.sendStatus(200);
+      res.sendStatus( 200 );
     }
-  });
-});
+  } );
+} );
 
-var listener = app.listen(process.env.PORT, function(){
-  console.log('Your bot is running on port ' + listener.address().port);
-});
+let listener = app.listen( process.env.PORT, function(){
+  console.log( 'Your bot is running on port ' + listener.address().port );
+} );
